86fc6727f62f85718bc1baacdb1790431a8a942b
fvieira <felipetavres@gmail.com>
2017-04-04 12:09:20 +0200

    working on ver perfil, design and UX

--- app/src/main/java/com/example/cya/boop/VerPerfil.java
+++ app/src/main/java/com/example/cya/boop/VerPerfil.java
@@ -5,10 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.cya.boop.core.Usuario;
+import com.example.cya.boop.util.LetterAvatar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -33,7 +38,11 @@ public class VerPerfil extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_ver_perfil);
+        getSupportActionBar().hide();
 
         //Si le viene un userID en los extras del intent, muestra los datos de ese senior, si no los del propio usuario actual
         if(getIntent().getExtras()==null) {
@@ -67,6 +76,9 @@ public class VerPerfil extends AppCompatActivity {
                     nombre = (TextView) findViewById(R.id.VPnombre);
                     nombre.setText(user.getNombre());
 
+                    ImageView avatar = (ImageView) findViewById(R.id.VPavatar);
+                    avatar.setImageDrawable(new LetterAvatar(VerPerfil.this, 0,"S",90));
+
                     edad = (TextView) findViewById(R.id.VPedad);
                     //Si eso, usar cosas no deprecated
                     edad.setText(getAge(user.getFechaNac().getYear()+1900, user.getFechaNac().getMonth(),user.getFechaNac().getDay()));
@@ -79,8 +91,17 @@ public class VerPerfil extends AppCompatActivity {
             }
         });
 
+        ImageButton botonSalir = (ImageButton) findViewById(R.id.VPsalir);
+        botonSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        /*
         botonLogout = (Button) findViewById(R.id.VPlogout);
         botonEditarPerfil = (Button) findViewById(R.id.VPmodificarPerfil);
+
 
         if(esMiPerfil) {
             botonLogout.setOnClickListener(new View.OnClickListener() {
@@ -101,8 +122,9 @@ public class VerPerfil extends AppCompatActivity {
             botonLogout.setVisibility(View.INVISIBLE);
             botonEditarPerfil.setVisibility(View.INVISIBLE);
         }
-
+        */
         TextView nombre = (TextView) findViewById(R.id.VPnombre);
+
 
     }
 
